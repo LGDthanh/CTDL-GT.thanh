bf0470e4bab7414fda8409ac4bf30d0bab6d53aa
Nguyễn Phúc Thanh <125104908+LGDthanh@users.noreply.github.com>
2023-05-26 15:06:47 +0700

    Lesson8

--- Lesson8.cpp
+++ Lesson8.cpp
@@ -33,31 +33,103 @@
 //     return 0;
 // }
 // Bài 1: Viết chương trình nhập vào 1 số nguyên dương n. kiểm tra xem số n có phải là số nguyên tố hay không.
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-void input_int(string label, int &n){
-    cout << label;
-    cin >> n;
-}
-bool kt_nt(int n){
-    for(int i = 2; i<=sqrt(n);i++){
-        if(n%i == 0){
-            return false;
-            }
-    }
-        return true;
+// void input_int(string label, int &n){
+//     cout << label;
+//     cin >> n;
+// }
+// bool kt_nt(int n){
+//     for(int i = 2; i<=sqrt(n);i++){
+//         if(n%i == 0){
+//             return false;
+//             }
+//     }
+//         return true;
 
-}
+// }
 
-int main() {
-    int n;
-    input_int(" n = ", n);
-    if(kt_nt(n)){
+// int main() {
+//     int n;
+//     input_int(" n = ", n);
+//     if(kt_nt(n)){
 
-        cout << " la so nguyen to." << endl;
-    } else{
-        cout << "khong phai la so nguyen to." << endl;
-    }
-    return 0;
-}
+//         cout << " la so nguyen to." << endl;
+//     } else{
+//         cout << "khong phai la so nguyen to." << endl;
+//     }
+//     return 0;
+// }
+// Bài 2: viết chương trình nhập vào 1 số nguyên dương n (n>1). Tính và in ra màn hình tổng các số nguyên tố từ 2 -> n. (sử dụng đệ quy)
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+//  void input_int(string label, int &n){
+//     cout << label;
+//     cin >> n;
+// }
+
+// bool kt_nt(int x){
+//     for(int i = 2; i<=sqrt(x); i++){
+//     if(x%i == 0){
+//         return false;
+//     }
+//     }
+//     return true;
+// }
+// int sum_sont(int n){
+//     int sum=0;
+//     for(int i = 2; i<=n; i++){
+//         if(kt_nt(i)){
+//             sum += i;
+//             }
+//             }
+//             return sum;
+// }
+// int sum_sont2( int x){
+//     if(x==2){
+//         return 2;
+//     }
+//     if(kt_nt(x)){
+//         return x + sum_sont2(x-1);
+//     }
+//     return sum_sont2(x-1);
+// }
+// int main() {
+//     int n;
+//     input_int(" n = ", n);
+//     cout<< sum_sont2(n);
+//     return 0;
+// }
+// Bài 3: Viết chương trình nhập vào 1 số nguyên dương n. Tính và in ra màn hình n!
+// #include <bits/stdc++.h>
+
+// using namespace std;
+//  void input_int(string label, int &n){
+//     cout << label;
+//     cin >> n;
+// }
+
+// int sum_n_gt(int x){
+//     if(x == 1){
+//         return 1;
+//     }
+//     return x*sum_n_gt(x-1);
+// }
+// long long sum_n_gt2(int x){
+//     int giaithua=1;
+//     for(int i = 1; i<=x;i++){
+//         giaithua *=i;
+//     }
+//     return giaithua;
+// }
+
+// int main() {
+//     int n;
+//     input_int("n = ", n);
+    
+//     cout << sum_n_gt2(n);
+//     return 0;
+// }
